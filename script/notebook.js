8a0944e897989383c3254647ea67cfe0b8d42dd9
Agustin Ruatta <agustinruatta@gmail.com>
2025-05-19 22:42:38 -0300

    Agregados comentarios

--- script/notebook.js
+++ script/notebook.js
@@ -31,17 +31,27 @@ function permitirAgregarFavoritos(idNotebook) {
     document.getElementById('agregar-favoritos').addEventListener('click', function () {
         let valorGuardadoLocalStorage = localStorage.getItem('favoritos');
 
+        //Si el valor guardado es null, es que no hay nada guardado en el localStorage
         if (valorGuardadoLocalStorage === null) {
+            //Como no hay nada guardado, creo una lista con el id del notebook que voy a agregar a favoritos.
             let listaFavoritos = [idNotebook];
+
+            //Lo guardo en el localStorage. Recordar que el localStorage solo guarda strings, por eso uso JSON.stringify para convertir el array en un string.
             localStorage.setItem('favoritos', JSON.stringify(listaFavoritos));
         } else {
+            //En este caso hay algo guardado en el localStorage. Lo que hago es obtener el valor guardado (que es un string) y convertirlo a un array.
             let listaFavoritos = JSON.parse(valorGuardadoLocalStorage);
+
+            //A esa lista, le agrego el id del notebook que quiero agregar a favoritos.
             listaFavoritos.push(idNotebook);
+
+            //Lo guardo en el localStorage. Recordar que el localStorage solo guarda strings, por eso uso JSON.stringify para convertir el array en un string.
             localStorage.setItem('favoritos', JSON.stringify(listaFavoritos));
         }
     })
 }
 
+//Obtengo los datos de la API
 let datosApi = await obtenerDatosApi();
 cargarDatosHtml(datosApi);
 permitirAgregarFavoritos(datosApi.id);
