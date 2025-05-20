200d2b04e22c6c5d6a5cc4218938436f74ca4785
Agustin Ruatta <agustinruatta@gmail.com>
2025-05-19 22:32:55 -0300

    LLamado a API

--- script/alta_notebook.js
+++ script/alta_notebook.js
@@ -1,12 +1,37 @@
-document.getElementById('formulario-carga').addEventListener('submit', function(event) {
+document.getElementById('formulario-carga').addEventListener('submit', async function(event) {
     event.preventDefault();
 
     let data = new FormData(document.getElementById('formulario-carga'));
 
-    alert(data.get('title'));
-    alert(data.get('description'));
-    alert(data.get('image'));
-    alert(data.get('site'));
-    alert(data.get('nameFeatureA'));
-    alert(data.get('descriptionFeatureA'));
+    let caracteristicas = [];
+
+    caracteristicas.push({
+        name: data.get('nameFeatureA'),
+        description: data.get('descriptionFeatureA')
+    });
+
+    caracteristicas.push({
+        name: data.get('nameFeatureB'),
+        description: data.get('descriptionFeatureB')
+    });
+
+    let object = {
+        title: data.get('title'),
+        description: data.get('description'),
+        image_url: data.get('image'),
+        factory_url: data.get('site'),
+        features: caracteristicas
+    }
+
+    let url = 'https://crudcrud.com/api/ca7e4cc6c4864843a05765fbca338813/notebook';
+
+    await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(object),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    alert('Producto agregado con éxito');
 })
